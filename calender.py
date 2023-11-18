97febb76a0ea37f9324b86d098601726ce3f354d
ahmed190777 <142106692+ahmed190777@users.noreply.github.com>
2023-11-18 06:23:36 +0200

    Add files via upload

---
+++ calender.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[42]:
+
+
+import calendar
+
+month=int(input("please enter the month :"))
+year=int(input("please enter the year :"))
+
+
+# In[49]:
+
+
+calendar.prmonth(year, month, w=0, l=0)
+
+
+# In[ ]:
+
+
+
+
