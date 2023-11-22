797c839e71a2c19ecb9d4f237a65ba28b6b08179
ahmed190777 <142106692+ahmed190777@users.noreply.github.com>
2023-11-22 20:53:37 +0200

    Add files via upload

---
+++ max_in _list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+x=[1,8,4,6,74,5,9,99,8,8,4,5,2,44,2,101,4,5,44,]
+
+
+# In[8]:
+
+
+max=-111111
+for i in range (len(x)):
+    if x[i]>max:
+        max=x[i]
+print(max)        
+
+
+# In[ ]:
+
+
+
+
